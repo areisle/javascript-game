6a4560c4cc36db1e32afd3bd77835254c408f70d
Abbey Reisle <A01001425@ad.bcit.ca>
2016-10-28 16:02:30 -0700

    switching

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,11 @@
 var items_collected = 0;
 
 $items = $('.items');
-$items.sortable({connectWith: ".cut"});
+$items.sortable({revert:0});
 //add ingredients to bag when clicked on
 $('.wrapper').on('click', '.ingredient', function (){
 	//append clicked ingredient to items list (opt: if list contains less than 12 items)
-	console.log("ingredient " + $(this).attr("id") + " was clicked");
+	//console.log("ingredient " + $(this).attr("id") + " was clicked");
 	appendItem(this);
 });
 
@@ -21,9 +21,54 @@ function appendItem(item) {
 	if (items_collected < 10) {
 		// <div class="item ingredient"> ingredient <button class="trash">remove</button></div>
 		var $current_item = $('<div class="item ' + $(item).attr('id') + '"><button class="trash">remove</button>' + $(item).attr('id') + '</div>');
+		var name = $(item).attr('id'),
+				state = 0,
+				stage = "unknown",
+				cut = false;
+
 		$items.append($current_item);
-        //add ingredient data
-		$current_item.data(new ingredient($(item).attr('id'), 'raw', 'unknown', 'unknown'));
+    //add ingredient data
+    // check ingredient type cut=true for onions, garlic, mushrooms
+    // possible stages - soffrito, tostatura, deglaze, cottura, mantecatura
+    switch(name) {
+    case "onions":
+    	cut = true;
+    	stage = "soffrito";
+    	break;
+    case "mushrooms":
+    	cut = true;
+    	stage = "soffrito";
+    	break;
+    case "garlic":
+    	cut = true;
+    	stage = "soffrito";
+    	break;
+    case "wine":
+    	cut = false;
+    	stage = "deglaze";
+    	break;
+    case "parmesan":
+    	cut = false;
+    	stage = "mantecatura";
+    	break;
+    case "butter":
+    	cut = false;
+    	stage = "soffrito";
+    	break;
+    case "rice":
+    	cut = false;
+    	stage = "tostatura";
+    	break;
+    case "stock":
+    	cut = false;
+    	stage = "cottura";
+    	break;
+    default:
+    	cut = false;
+    	break;
+    }
+
+		$current_item.data(new ingredient(name, state, cut, stage));
 		//increment
 		items_collected++;
 		console.log($items.find('.item:last-child').data());
@@ -44,12 +89,12 @@ function removeItem(item) {
 	items_collected--;
 }
 
-function ingredient(name, state, type, stage) {
+function ingredient(name, state, cut, stage) {
 	//ex. name = "onion"
 	this.name = name;
 	//ex. type = "cut"
 	// may use for other thing eventually
-	this.type = type;
+	this.cut = cut;
 	// the stage in cooking when this ingredient should be added (likely expressed as a time)
 	this.stage = stage;
 	// state of the ingredient
@@ -84,24 +129,67 @@ function navigate(href) {
 	$view.load('views.html ' + href + '-view', function() {makeDroppable(view_name)});
 	//handle adding droppable if view=cutting-board
 	
-	console.log('href: '+href+', $view: '+$view+', view_name: '+view_name);
+	//console.log('href: '+href+', $view: '+$view+', view_name: '+view_name);
 }
+
+
+/* -----------------------
+
+  cutting board - making droppable, checking cut type etc.
+
+  -------------------------*/
 function makeDroppable(view_name) {
-    console.log("function droppable called");
+    //console.log("function droppable called");
     if (view_name === 'cutting-board') {
-        console.log("cut: "+ $(".cut"));
-        $('.cut').droppable({
-            accept: '.item',
+        //console.log("cut: "+ $(".cut"));
+        $('.cut.brunoise').data({"cut": 3});
+        $('.cut.brunoisette').data({"cut": 4});
+        $('.cut.vichy').data({"cut": 1});
+        $('.cut.julienne').data({"cut": 2});
+        //console.log("cut data: " + $('.cut.julienne').data("cut"));
+        $('.cut.julienne').droppable({
+            accept: checkCut,
             drop: handleDropEvent,
             hoverClass: 'drop-hover',
             tolerance: 'pointer'
           });
+        $('.cut.brunoisette').droppable({
+            accept: checkCut,
+            drop: handleDropEvent,
+            hoverClass: 'drop-hover',
+            tolerance: 'pointer'
+          });
+        $('.cut.vichy').droppable({
+            accept: checkCut,
+            drop: handleDropEvent,
+            hoverClass: 'drop-hover',
+            tolerance: 'pointer'
+          });
+        $('.cut.brunoise').droppable({
+            accept: checkCut,
+            drop: handleDropEvent,
+            hoverClass: 'drop-hover',
+            tolerance: 'pointer'
+          });
+
+        //occurs when something is dropped
         function handleDropEvent(event, ui) {
           ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
           //setTransformOrigin($(this).parent()[0], ui.draggable[0]);
           //ui.draggable.css("transform",'rotateX(45deg)');
           //$(this).parent().
-          ui.draggable.html("dropped");
+          console.log("item state: " + ui.draggable.data("state") + ", cut state: " + $(this).data("cut"));
+          //console.log("ui: " + event.data("name"));
+          ui.draggable.append("<p>cut type: " + $(this).data("cut") +"</p>");
+          ui.draggable.data("state", $(this).data("cut"));
+          //$(ui.draggable).draggable({ revert: false});
         }
     }
+}
+function checkCut(e) {
+	if (e.data("cut") === true && e.data("state") < $(this).data("cut") ) {
+		return true;
+	} else {
+		return false;
+	}
 }
