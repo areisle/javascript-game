8c9fce7974c66c7f3b41c9fff4d986acdded0247
areisle <areisle@gmail.com>
2016-10-31 22:44:50 -0700

    OMG it works

--- scripts/main.js
+++ scripts/main.js
@@ -16,10 +16,12 @@ $(document).ready(function () {
         startTime = "new",
         animationEvent = whichAnimationEvent(),
         $body = $('body'),
-    //have different links available based on state of game
+        //have different links available based on state of game
         game_state = {"locked": false, "cooking": false},
-        $items = $('.items');
-    function Ingredient(name, state, cut, stage) {
+        $items = $('.items'),
+        score = {'cutting':0,'timing':0};
+    
+    function Ingredient(name, state, cut, stage, time) {
         //ex. name = "onion"
         this.name = name;
         //ex. type = "cut"
@@ -32,6 +34,8 @@ $(document).ready(function () {
         // 1-4 for the different cuts
         // have 1 be largest and 4 smallest
         this.state = state;
+        // time at which ingredient should be added
+        this.time = time
     }
 
     function appendItem(item) {
@@ -43,10 +47,10 @@ $(document).ready(function () {
                 name = $(item).attr('id'),
                 state = 0,
                 stage = "unknown",
-                cut = false;
+                cut = false,
+                time;
             collection_counts[name]++;
-            $items.append($current_item);
-            $current_item
+            $items.prepend($current_item);
             //add ingredient data
             // check ingredient type cut=true for onions, garlic, mushrooms
             // possible stages - soffrito, tostatura, deglaze, cottura, mantecatura
@@ -54,51 +58,67 @@ $(document).ready(function () {
             case "onions":
                 cut = true;
                 stage = "soffrito";
+                time = 6000;
                 break;
             case "mushrooms":
                 cut = true;
                 stage = "soffrito";
+                time = 8000;
                 break;
             case "garlic":
                 cut = true;
                 stage = "soffrito";
+                time = 12000;
                 break;
             case "wine":
                 cut = false;
                 stage = "deglaze";
+                time = 2000;
                 break;
             case "parmesan":
                 cut = false;
                 stage = "mantecatura";
+                time = 4000;
                 break;
             case "butter":
                 cut = false;
                 stage = "soffrito";
+                time = 4000;
                 break;
             case "rice":
                 cut = false;
                 stage = "tostatura";
+                time = 2500;
                 break;
             case "stock":
                 cut = false;
                 stage = "cottura";
+                time = 3000;
                 break;
             default:
                 cut = false;
                 break;
             }
 
-            $current_item.data(new Ingredient(name, state, cut, stage));
+            $current_item.data(new Ingredient(name, state, cut, stage, time));
             //increment
             items_collected++;
-            console.log($items.find('.item:last-child').data());
+            console.log($items.find('.item:first-child').data());
         } else {
             //temporary alert -- beautify later
             alert("sorry your bag is full");
         }
         //for debugging
     }
-
+    function getFinalScore() {
+        for (var item in collection_counts){
+            if (collection_counts[item] < 1){
+                return 0;
+            }
+        }
+        var finalScore = (score.cutting + score.timing)/12*100;
+        return finalScore;
+    }
     function removeItem(item) {
         //remove the item --done
         //remove ingredient to ingredients_collected
@@ -134,8 +154,7 @@ $(document).ready(function () {
         $('.pop-up').remove();
         $('.pop-up-overlay').remove(); 
     }
-
-    $items.sortable();
+    $items.sortable({containment: '.items'});
     //add ingredients to bag when clicked on
     $('.wrapper').on('click', '.ingredient', function () {
         //append clicked ingredient to items list (opt: if list contains less than 12 items)
@@ -167,9 +186,7 @@ $(document).ready(function () {
     function potView() {
         // first check they have at least one of every ingredient
         //temporary so I can skip collecting ingredients
-        var next_animation = 'flash-and-fill-copy 5s linear',
-            on_copy = false;
-        if (true || collectionCheck()){
+        if (collectionCheck()){
             //had all ingredients
             //make pop up ask if they're sure
             //try to turn off other navigation while pop-p is up (maybe overlay div that fade in and reset its pointer-events to true or whatever)
@@ -184,8 +201,21 @@ $(document).ready(function () {
                     removePopUp();
                     //add next pop-up ---first of the cooking stages
                     //pop-up soffrito
+                    //use this to make sure all ingredients get added
+                    collection_counts = {"onions": 0,
+                             "garlic": 0,
+                             "wine": 0,
+                             "mushrooms": 0,
+                             "rice": 0,
+                             "parmesan": 0,
+                             "butter": 0,
+                             "stock": 0}
                     popUpLoop();
                 });
+            });
+            $body.on('click', '.pop-up button.no', function() {
+                removePopUp();
+                //do nothing
             });
         } else {
             //have pop up saying they still need more ingredients
@@ -204,7 +234,12 @@ $(document).ready(function () {
         if (cookingStages.length === 0) {
             console.log("ended");
             //end of game stuff
-            makePopUp(["the game is over! clikc okay to recieve your score"],['ok']);
+            var score = getFinalScore();
+            var message = ["the game is over!", "score: " + score + "%"];
+            if (score === 0) {
+                message.push("looks like you forgot to add something...");
+            }
+            makePopUp(message,['ok']);
             $body.one('click', '.pop-up button.ok', function(e) {
                 removePopUp();
                 //go to end of game screen
@@ -219,21 +254,23 @@ $(document).ready(function () {
                 copy = 1;
             }
             console.log(cookingStages[0]);
+            currentStage = cookingStages[0];
             makePopUp([cookingStages.shift()],['start']);
-
+            
             $body.one('click', '.pop-up button.start', function(e) {
                 removePopUp();
                //set up timer
                 var $pie = $('.pie-timer circle');
+                startTime = new Date().getTime();
                 $pie.css({'animation-play-state':'running'});
                 //trigger next pop-up on timer end
                 $body.one(animationEvent, '.pie-timer circle', function(){
                     //reset animation -- trigger reflow by changing animation name
                     $pie.css('animation-play-state','paused');
                     if (copy===1) {
-                       $pie.css('animation', 'flash-and-fill-copy 3s linear'); 
+                       $pie.css('animation', 'flash-and-fill-copy 5s linear'); 
                    } else {
-                        $pie.css('animation', 'flash-and-fill 3s linear');
+                        $pie.css('animation', 'flash-and-fill 5s linear');
                    }
                     $pie.css('animation-play-state','paused');
                     console.log("animation end");
@@ -260,24 +297,10 @@ $(document).ready(function () {
 
       -------------------------*/
     function makeDroppable(view_name) {
+        $items.sortable('option','containment','.items');
         if (view_name === 'cutting-board') {
+            $items.sortable('option','containment','window');
             $('.cut.julienne')
-                .data({"cut": 2})
-                .droppable({
-                    accept: checkCut,
-                    drop: handleDropEvent,
-                    hoverClass: 'drop-hover',
-                    tolerance: 'pointer'
-              });
-            $('.cut.brunoisette')
-                .data({"cut": 4})
-                .droppable({
-                    accept: checkCut,
-                    drop: handleDropEvent,
-                    hoverClass: 'drop-hover',
-                    tolerance: 'pointer'
-              });
-            $('.cut.vichy')
                 .data({"cut": 1})
                 .droppable({
                     accept: checkCut,
@@ -285,8 +308,16 @@ $(document).ready(function () {
                     hoverClass: 'drop-hover',
                     tolerance: 'pointer'
               });
-            $('.cut.brunoise')
+            $('.cut.brunoisette')
                 .data({"cut": 3})
+                .droppable({
+                    accept: checkCut,
+                    drop: handleDropEvent,
+                    hoverClass: 'drop-hover',
+                    tolerance: 'pointer'
+              });
+            $('.cut.brunoise')
+                .data({"cut": 2})
                 .droppable({
                     accept: checkCut,
                     drop: handleDropEvent,
@@ -295,6 +326,7 @@ $(document).ready(function () {
               });
 
         } else if (view_name==="pot") {
+            $items.sortable('option','containment','window');
             //add pot to drop ingredients into
             //conditions for starting cooking --clicked okay, have all ingredients
             $('.pot.cooking').droppable({
@@ -306,26 +338,22 @@ $(document).ready(function () {
         }
     }
 
-    //occurs when item is added to the list
+    //occurs when item is dropped on cutting board
     function handleDropEvent(event, ui) {
-      ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+      //ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
       console.log("item state: " + ui.draggable.data("state") + ", cut state: " + $(this).data("cut"));
       //console.log("ui: " + event.data("name"));
-      ui.draggable.append("<p>cut type: " + $(this).data("cut") +"</p>");
-      ui.draggable.data("state", $(this).data("cut"));  
+        var cut = $(this).data("cut");
+      //ui.draggable.append("<p>cut type: " + cut +"</p>");
+      ui.draggable.data("state", cut);
+        ui.draggable.css('background-position', 33.3*cut + '%');
     }
 
     function handleCookingDropEvent(event, ui) {
-        console.log("entered handleCookingDropEvent function");
         //once item is added, remove istead of revert
         //have counter checking num added so far
-          if(startTime==="new") {
-            startTime = new Date().getTime();
-          } else {
-            var timeDropped = new Date().getTime();
-            console.log( "Time difference: " + ( timeDropped- startTime) );
-            startTime = timeDropped;
-          }
+        var timeDropped = new Date().getTime() -startTime;
+        updateScore(ui.draggable,timeDropped);
         ui.draggable.remove();
     }
 
@@ -355,5 +383,71 @@ $(document).ready(function () {
             }
         }
     }
+    
+    function updateScore(item, time) {
+        //check stage
+        console.log(currentStage);
+        switch(currentStage) {
+            case 'soffrito':
+                if ($(item).data().name === 'onions') {
+                    //onion added
+                    console.log('onion added');
+                    if ($(item).data().state === 2){
+                        //full points on cut
+                        score.cutting += 1;
+                    }
+                    
+                } else if ($(item).data().name === 'garlic') {
+                    //garlic added
+                    console.log('garic added');
+                    if ($(item).data().state === 3){
+                        //full points on cut
+                        score.cutting += 1;
+                    }
+                } else if ($(item).data().name === 'mushrooms') {
+                    //mushrooms added
+                    console.log('mushroom added');
+                    if ($(item).data().state === 1){
+                        //full points on cut
+                        score.cutting += 1;
+                    }
+                } else if ($(item).data().name === 'butter') {
+                    //butter added
+                    console.log('butter added');
+                }
+                break;
+            case 'tostatura':
+                if ($(item).data().name === 'rice') {
+                    //rice added
+                }
+                break;
+            case 'deglaze':
+                if ($(item).data().name === 'wine') {
+                    //wine added
+                }
+                break;
+            case 'cottura':
+                if ($(item).data().name === 'stock') {
+                    //stock added
+                }
+                break;
+            case 'mantecatura':
+                if ($(item).data().name === 'parmesan') {
+                    //parmesan added
+                    if ($(item).data('state') === 1) {
+                        //parm was grated
+                    }
+                }
+                break;
+        }
+        if (currentStage === $(item).data().stage) {
+            score.cutting += (1 - Math.abs($(item).data().time - time)/15000);
+           
+        }
+        collection_counts[$(item).data().name]++;
+        console.log('cutting score: ' + score.cutting);
+    }
+    
+    
 });
 
